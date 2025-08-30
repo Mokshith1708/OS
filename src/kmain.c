b3cb04b082bb405918477892f9238d179fb6916d
IPKCHK <istarlakalyananudeep@gmail.com>
2025-08-30 09:06:44 +0000

    Committed

--- src/kmain.c
+++ src/kmain.c
@@ -1,12 +1,17 @@
 #include "hal/hal_console.h"
-
-/*
- * This is the main entry point for the C portion of our kernel.
- * It is called by the Reset_Handler in boot.s after memory is initialized.
- */
-
+#include "pmm.h"
 int kmain(void) {
     hal_console_puts("Project Monolith Kernel Initialized.\n");
-    while (1) { }
+    hal_console_puts("--- Module 2 Test ---\n");
+    pmm_init();
+    uintptr_t user_base = pmm_get_user_space_base();
+    size_t user_size = pmm_get_user_space_size();
+    size_t user_size_kb = user_size / 1024;
+    hal_console_puts("User space available: ");
+    hal_console_put_int((int)user_size_kb);
+    hal_console_puts("KB at ");
+    hal_console_put_hex((uint32_t)user_base);
+    hal_console_puts("\n");
+    while (1) {}
     return 0;
 }

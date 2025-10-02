286bfd22ed0ccda9b60235badbcd4d2ed0a997ed
ethicode4-ops <ethicode4@gmail.com>
2025-10-02 22:11:00 +0530

    feat: working vga + ps2 ;)

--- src/kmain.c
+++ src/kmain.c
@@ -23,7 +23,7 @@
 
 static XScuGic Intc;
 
-static int SetupIntrSystem(XAxiVdma *AxiVdmaPtr, u16 ReadIntrId);
+static int SetupIntrSystem(XAxiVdma *AxiVdmaPtr, u16 VdmaIntrId, axi_ps2 *Ps2Ptr, u16 Ps2IntrId);
 void PS2_InterruptHandler(void *CallBackRef);
 
 volatile unsigned char Buffer[FrameSize];
@@ -33,7 +33,6 @@ XGpioPs Gpio;
 FATFS fatfs;
 FRESULT res;
 static axi_ps2 Ps2Inst;
-//static XScuGic Intc;
 
 int main()
 {
@@ -136,7 +135,17 @@ int main()
 
     XAxiVdma_IntrEnable(&myVDMA, XAXIVDMA_IXR_COMPLETION_MASK, XAXIVDMA_READ);
 
-    SetupIntrSystem(&myVDMA, XPAR_FABRIC_AXI_VDMA_0_MM2S_INTROUT_INTR);
+    //Initialize PS2
+    axi_ps2_Config *ConfigPtr;
+
+    ConfigPtr = axi_ps2_LookupConfig(XPAR_AXI_PS2_0_DEVICE_ID);
+    if (ConfigPtr == NULL) {
+        return XST_FAILURE;
+    }
+    axi_ps2_CfgInitialize(&Ps2Inst, ConfigPtr, ConfigPtr->BaseAddress);
+
+    SetupIntrSystem(&myVDMA, XPAR_FABRIC_AXI_VDMA_0_MM2S_INTROUT_INTR,&Ps2Inst,XPAR_FABRIC_AXI_PS2_0_PS2_INTERRUPT_INTR);
+
     Xil_DCacheFlush();
 
     status = XAxiVdma_DmaStart(&myVDMA, XAXIVDMA_READ);
@@ -147,41 +156,11 @@ int main()
         return XST_FAILURE;
     }
 
-    // Initialize PS2
-    axi_ps2_Config *ConfigPtr;
-
-    ConfigPtr = axi_ps2_LookupConfig(XPAR_AXI_PS2_0_DEVICE_ID);
-    if (ConfigPtr == NULL) {
-    	return XST_FAILURE;
-    }
-    axi_ps2_CfgInitialize(&Ps2Inst, ConfigPtr, ConfigPtr->BaseAddress);
-
-    XScuGic Intc;
-    XScuGic_Config *IntcConfig;
-
-    IntcConfig = XScuGic_LookupConfig(XPAR_PS7_SCUGIC_0_DEVICE_ID);
-    if (XScuGic_CfgInitialize(&Intc, IntcConfig, IntcConfig->CpuBaseAddress) != XST_SUCCESS) {
-        xil_printf("Interrupt controller initialization failed\n");
-        return XST_FAILURE;
-    }
-    int Status;
-    Status = XScuGic_Connect(&Intc, XPAR_FABRIC_AXI_PS2_0_PS2_INTERRUPT_INTR,
-                             (Xil_InterruptHandler)axi_ps2_IntrHandler,
-                             &Ps2Inst);
-    if (Status != XST_SUCCESS) {
-        xil_printf("PS/2 Interrupt connection failed\n");
-        return XST_FAILURE;
-    }
-
-    XScuGic_Enable(&Intc, XPAR_FABRIC_AXI_PS2_0_PS2_INTERRUPT_INTR);
-
     axi_ps2_SetHandler(&Ps2Inst, PS2_InterruptHandler, &Ps2Inst);
 
     axi_ps2_IntrEnable(&Ps2Inst, axi_ps2_IPIXR_RX_ALL);  // Enable receive interrupts
 
     axi_ps2_IntrGlobalEnable(&Ps2Inst); // Enable global interrupt in the IP
-
-
 
 
     shell_run(); //  never return
@@ -195,7 +174,8 @@ int main()
  ******************************************************************************/
 static void ReadCallBack(void *CallbackRef, u32 Mask)
 {
-    Xil_DCacheFlushRange((INTPTR)Buffer, FrameSize);
+//    xil_printf("Read Callback\r\n");
+	Xil_DCacheFlushRange((INTPTR)Buffer, FrameSize);
 }
 
 static void ReadErrorCallBack(void *CallbackRef, u32 Mask)
@@ -204,42 +184,50 @@ static void ReadErrorCallBack(void *CallbackRef, u32 Mask)
     xil_printf("Read Call back Error function is called\r\n");
 }
 
-static int SetupIntrSystem(XAxiVdma *AxiVdmaPtr, u16 ReadIntrId)
+static int SetupIntrSystem(XAxiVdma *AxiVdmaPtr, u16 VdmaIntrId, axi_ps2 *Ps2Ptr, u16 Ps2IntrId)
 {
     int Status;
-    XScuGic *IntcInstancePtr = &Intc;
 
     /* Initialize the interrupt controller and connect the ISRs */
     XScuGic_Config *IntcConfig;
     IntcConfig = XScuGic_LookupConfig(XPAR_PS7_SCUGIC_0_DEVICE_ID);
-    Status = XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig, IntcConfig->CpuBaseAddress);
+    Status = XScuGic_CfgInitialize(&Intc, IntcConfig, IntcConfig->CpuBaseAddress);
     if (Status != XST_SUCCESS)
     {
         xil_printf("Interrupt controller initialization failed..");
         return -1;
     }
 
-    Status = XScuGic_Connect(IntcInstancePtr, ReadIntrId, (Xil_InterruptHandler)XAxiVdma_ReadIntrHandler, (void *)AxiVdmaPtr);
+    // Connect VDMA interrupt
+    Status = XScuGic_Connect(&Intc, VdmaIntrId, (Xil_InterruptHandler)XAxiVdma_ReadIntrHandler, (void *)AxiVdmaPtr);
     if (Status != XST_SUCCESS)
     {
         xil_printf("Failed read channel connect intc %d\r\n", Status);
         return XST_FAILURE;
     }
+    XScuGic_Enable(&Intc, VdmaIntrId);
 
-    XScuGic_Enable(IntcInstancePtr, ReadIntrId);
+    // Connect PS2 interrupt
+    Status = XScuGic_Connect(&Intc, Ps2IntrId, (Xil_InterruptHandler)axi_ps2_IntrHandler, (void *)Ps2Ptr);
+    if (Status != XST_SUCCESS)
+    {
+        xil_printf("PS2 interrupt connect failed %d\r\n", Status);
+        return XST_FAILURE;
+    }
+    XScuGic_Enable(&Intc, Ps2IntrId);
 
-    Xil_ExceptionInit();
-    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler, (void *)IntcInstancePtr);
-    Xil_ExceptionEnable();
-
-    /* Register call-back functions
-     */
+    /* Register call-back functions for VDMA */
     XAxiVdma_SetCallBack(AxiVdmaPtr, XAXIVDMA_HANDLER_GENERAL, ReadCallBack, (void *)AxiVdmaPtr, XAXIVDMA_READ);
-
     XAxiVdma_SetCallBack(AxiVdmaPtr, XAXIVDMA_HANDLER_ERROR, ReadErrorCallBack, (void *)AxiVdmaPtr, XAXIVDMA_READ);
+
+    /* Initialize exception handling */
+    Xil_ExceptionInit();
+    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler, &Intc);
+    Xil_ExceptionEnable();
 
     return XST_SUCCESS;
 }
+
 
 void PS2_InterruptHandler(void *CallBackRef)
 {
